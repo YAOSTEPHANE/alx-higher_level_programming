6dda2a55c22f089cc587f01dd95feb465ec27b98
YAOSTEPHANE <106774656+YAOSTEPHANE@users.noreply.github.com>
2023-01-17 17:51:37 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,27 +1,14 @@
 #!/usr/bin/python3
-
 """Sends a request to the URL and displays the body of the response."""
 
 
-
-
-
 if __name__ == '__main__':
+    from sys import argv
+    from requests import get
 
-        from sys import argv
+    url = argv[1]
 
-            from requests import get
-
-
-
-                url = argv[1]
-
-
-
-                    response = get(url)
-
-                        ERR_TXT = 'Error code: {}'
-
-                            status = response.status_code
-
-                                print(ERR_TXT.format(status) if (status >= 400) else response.text)
+    response = get(url)
+    ERR_TXT = 'Error code: {}'
+    status = response.status_code
+    print(ERR_TXT.format(status) if (status >= 400) else response.text)
